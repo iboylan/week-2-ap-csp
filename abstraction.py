f315e46b7676f0f4b0e97b42d6e86b655028d633
iboylan <pipiel326@gmail.com>
2025-11-04 18:48:13 +0000

    yay

---
+++ abstraction.py
@@ -0,0 +1,33 @@
+# abstraction -> when u hide the complex details of something
+# q1 = input("How old are you?:  ")
+# q2 = input("Where do you live?:  ")
+# q3 = input("What school do you go to?:  ")
+# print(q1 + q2 + q3)
+
+def agaaggaaga():
+    print("those who know")
+
+oobyschoopert = [1, 2, 3, 4, 5, 6, 7, 73829472382934, 5789248589438973548574, 11289219, 328484474433]
+
+print(len(oobyschoopert))
+agaaggaaga()
+
+for item in oobyschoopert:
+    agaaggaaga()
+
+mimimimi = {
+    1:"The Goop",
+    2:"the",
+    3:"Agagaghbfbhbhbhbhj jfhjfjhf",
+    4:"lelelelele",
+    5:"THE GOOP",
+    6:"schlime",
+    7:"Ue",
+    8:"AHHHHHHHHH",
+    9:"lelelelelelelelelel",
+    10:"The gop"
+
+}
+
+for x in mimimimi:
+    print(mimimimi[x])
